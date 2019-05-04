9984d9dcb482c86d69d7751309ff4d22e82e47d9
Ricardo <unrhs@hotmail.com>
2019-05-04 08:15:59 -0500

    profile 4 perfil

--- routes/business.js
+++ routes/business.js
@@ -3,10 +3,14 @@ const express = require("express");
 const router = express.Router();
 const Business = require("../models/Business");
 const uploader = require("../helpers/multer");
+const helpers = require("../helpers/function");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+
 
 module.exports = router;
--- routes/queue.js
+++ routes/queue.js
@@ -4,11 +4,20 @@ const User = require('../models/User'); // It refers to Company
 const Customer = require('../models/Customer');
 const uploader = require("../helpers/multer");
 const helpers = require("../helpers/function");
+const Business = require("../models/Business");
+
+const isAuth = (req, res, next) => {
+  if(req.isAuthenticated()){
+    next();
+  }else {
+    res.redirect("/auth/login")
+  }
+};
 
 
 // show queue for an authenticated user (company)
-router.get("/", helpers.isAuth, (req, res) => {
-  const { user } = req;
+router.get("/", isAuth, (req, res, next) => {
+  const {user} = req;
   Customer.find({businessID: user._id})
     .then(customers => {
       res.render("queue" , {customers});
@@ -18,12 +27,12 @@ router.get("/", helpers.isAuth, (req, res) => {
     });
 });
 
-router.get('/add',helpers.isAuth,(req,res) =>{
+router.get('/add',isAuth,(req,res) =>{
   res.render('queue')
 });
 
 
-router.post("/add", helpers.isAuth, (req, res) => {
+router.post("/add", isAuth, (req, res) => {
   let {_id:businessID} = req.user;
   customer = { businessID, ...req.body };
   console.log(customer);
